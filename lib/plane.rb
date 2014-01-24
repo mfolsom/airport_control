2e09dac92c83e454a8b147c4999b446b364c0e88
Megan Folsom <mfolsom@gmail.com>
2014-01-24 11:30:36 +0000

    Add Airport class

--- lib/plane.rb
+++ lib/plane.rb
@@ -1,8 +1,25 @@
 class Plane
-	def flying?
-		true
+attr_accessor :flying, :landing
+
+	def initialize(status)
+		@flying = flying
+		@landing = landing
 	end
+
+	def landing
+		@landing = true
+	end
+
+	def flying
+		@flying = true 
+	end
+
 	def landing?
-		true
+		@landing 
+
 	end
+
+	def flying?
+		@flying 
+	end	
 end
--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -2,14 +2,21 @@ require 'spec_helper'
 
 describe Plane do 
 	it "can be flying" do
-		plane = Plane.new
+		flying = :flying
+		plane = Plane.new(flying)
 		expect(plane).to be_flying
 	end
 
 	it "can be landing" do
-		plane = Plane.new
+		landing =(:landing)
+		plane = Plane.new(landing)
 		expect(plane).to be_landing
 	end
 
+	it "cannot be flying and landing at the same time" do
+		landing = (:landing)
+		plane = Plane.new(landing)
+		expect(plane.flying?).to eq(false)
+	end
 
 end
