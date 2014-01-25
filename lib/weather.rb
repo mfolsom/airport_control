34f78d50cc4115b0de8aef6c2aaba161c0316c3e
Megan Folsom <mfolsom@gmail.com>
2014-01-25 11:38:53 +0000

    Add unpredictable conditions

--- lib/weather.rb
+++ lib/weather.rb
@@ -3,5 +3,7 @@ module Weather
 	def conditions
 		@conditions = :sunny
 	end
-
+	def unpredictable_conditions
+		@conditions = :stormy
+	end
 end
--- spec/weather_spec.rb
+++ spec/weather_spec.rb
@@ -3,9 +3,13 @@ require 'spec_helper'
 class Today; include Weather; end
 
 describe Weather do
-it "should be somewhat predictable" do
-	today = Today.new
-	expect(today.conditions).to be(:sunny)
+	it "should be somewhat predictable" do
+			today = Today.new
+			expect(today.conditions).to be(:sunny)
+	end
 
-end
+	it "should surprise everyone with random bad weather" do
+		today = Today.new
+		expect(today.unpredictable_conditions).to be(:stormy)
+	end
 end
